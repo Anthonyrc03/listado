3140b880c81fa0e267550f2144204bba6517f435
AlbertPerez23 <salbert_7@hotmail.com>
2023-08-23 18:50:25 -0300

    Funcion para actualizar la vista mas el almacenamiento local

--- listado/js/almacenar.js
+++ listado/js/almacenar.js
@@ -7,7 +7,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const limpiarBtn = document.getElementById("limpiar");
   
     // Cargar el listado desde el almacenamiento local al iniciar
-    const listadoGuardado = JSON.parse(localStorage.getItem("listado")) || [];
+    const listadoGuardado = JSON.parse(localStorage.getItem("listado")) || [];   
+    
+    
+    // Función para actualizar la vista del listado
+    function actualizarVista() {
+      contenedor.innerHTML = ""; // Limpiar el contenido actual
   
-})
+      for (const item of listadoGuardado) {
+        const li = document.createElement("li");
+        li.textContent = item;
+        li.className = "list-group-item";
+        contenedor.appendChild(li);
+      }
+    }
+  
+  });
+  
+
   
